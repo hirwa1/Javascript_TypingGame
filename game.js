72d043de02136573c1193fc297785de6af694f48
NIYIBIZI HIRWA <61378957+hirwa1@users.noreply.github.com>
2021-02-21 05:41:00 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let quotes_array = [
   "Push yourself, because no one else is going to do it for you.",
   "Failure is the condiment that gives success its flavor.",
   "Wake up with determination. Go to bed with satisfaction.",
-  "It's going to be hard, but hard does not mean impossible.",
+  "It's going to be hard, but hard does not mean impossible things.",
   "Learning never exhausts the mind.",
   "The only way to do great work is to love what you do."
 ];
